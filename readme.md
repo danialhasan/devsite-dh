a9b367142966ace956d01f2d76d2a0ff8a42b31a
Danial Hasan <danialhasan14@gmail.com>
2021-04-19 20:57:31 -0400

    added netlify branch

--- readme.md
+++
@@ -1,3 +0,0 @@
-### Welcome to my portfolio site!
-
-I'm Danial, a full stack web developer. This is my portfolio site and has relevant projects and information about me. 
--- src/routes/404.ts
+++
@@ -1,9 +0,0 @@
-export{ }
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req: Request, res: any) => {
-    res.render('404')
-})
-
-module.exports = router;
